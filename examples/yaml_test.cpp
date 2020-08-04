792542c002049587786712f90026df180f74ebd4
Wei Jian <weiofjian@gmail.com>
2020-08-04 13:18:19 -0700

    alienware checkout

--- examples/yaml_test.cpp
+++ examples/yaml_test.cpp
@@ -3,26 +3,36 @@
 #include <cassert>
 int main()
 { 
-    YAML::Node config = YAML::LoadFile("../examples/test.yaml");
-    if (config["pi"]) {
-    std::cout << "Last logged in: " << config["pi"].as<double>() << "\n";
+    YAML::Node config;
+    try{
+        config = YAML::LoadFile("../examples/test.yaml");
     }
+    catch(std::exception& e){
+        std::cout<< e.what() << std::endl;
+        return 1;
+    }
+    std::string str = config[0]["name"].as<std::string>();
+    std::cout<< str << std::endl;
+    // if (config["pi"]) {
+    // std::cout << "Last logged in: " << config["pi"].as<double>() << "\n";
+    // }
+    // int a = 1;
+    // auto pf = reinterpret_cast<float*>(&a);
+    // std::cout<< *pf << std::endl;
 
-    const std::string username = config["doe"].as<std::string>();
-    const std::string password = config["ray"].as<std::string>();
-    std::cout << username << std::endl;
+    // auto pi = reinterpret_cast<int*>(pf);
+    // std::cout<< *pi << std::endl;
+
+    // const std::string username = config["doe"].as<std::string>();
+    // std::cout << username << std::endl;
     
-    assert(config["calling-birds"].IsSequence());
-    printf("%zu\n", config["calling-birds"].size());
-    printf("%zu\n", config["calling-birds"]["good"].size() );
-    printf("%zu\n", config["calling-birds"][0].size() );
-    printf("%zu\n", config["calling-birds"][1].size() );
-    printf("%zu\n", config["calling-birds"][2].size() );
-    printf("%zu\n", config["calling-birds"][3].size() );
-    assert(config["another"].IsSequence());
-    printf("%zu\n", config["another"].size());
-    printf("%d\n",config["another"][0].as<int>() );
+    // assert(config["calling-birds"].IsSequence());
+    // printf("%zu\n", config["calling-birds"].size());
 
-    std::cout<< config["xmas-fifth-day"]["calling-birds"].as<std::string>() << std::endl; 
+    // assert(config["another"].IsSequence());
+    // printf("%zu\n", config["another"].size());
+    // printf("%d\n",config["another"][0].as<int>() );
+
+    // std::cout<< config["xmas-fifth-day"]["calling-birds"].as<std::string>() << std::endl; 
     return 0;
 }
